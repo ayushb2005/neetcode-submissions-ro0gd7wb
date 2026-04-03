86455473cfa9e3cf4d417fdc811007540d87f1e2
Ayush <73355805+ayushb2005@users.noreply.github.com>
2026-04-03 18:51:16 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,53 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        '''
+        find a 1 add it to a visited set
+        then we check left, right, up, down if 1 add to stack toVisit
+        then we check whicever direction and apply the same and add those 
+        into a visited set everytime we pop off stack. Once everything 
+        off stack is popped off continue going through the lists 
+        checking if there is a 1 AND it is not in a visited set
+
+        '''
+        visitedSet = set()
+        visitStack = [] #append, pop
+        islands = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if(grid[row][col] == "1" and (row,col) not in visitedSet):
+                    islands += 1
+                    visitStack.append((row,col))
+                    #DFS
+                    while(visitStack):
+                        remove = visitStack.pop()
+                        visitedSet.add(remove)
+                        self.checkNeighbors(grid, remove[0], remove[1], visitedSet, visitStack)
+                        #check neighbors
+                        #check bounds, land, not visited
+        return islands
+                        
+    def checkNeighbors(self, grid, row, col, visitedSet, visitStack):
+
+        dir = [(1,0), (-1,0), (0,1), (0,-1)]
+        for r,c in dir:
+            rowR = row+r
+            colC = col + c
+            if(0 <= rowR < len(grid) and 0<= colC < len(grid[0]) \
+            and grid[rowR][colC] == "1" and (rowR, colC) not in visitedSet):
+                visitStack.append((rowR,colC))
+        #[row+1][col], [row-1][col], [row][col+1], [row][col-1]
+        # if(0 <= row+1 < len(grid) and 0 <= col < len(grid[0]) \
+        # and grid[row+1][col] == "1" and (row+1, col) not in visitedSet):
+        #     visitStack.append((row+1,col))
+        # if(0 <= row-1 < len(grid) and 0 <= col < len(grid[0]) \
+        # and grid[row-1][col] == "1" and (row-1, col) not in visitedSet):
+        #     visitStack.append((row-1,col))
+        # if(0 <= row < len(grid) and 0 <= col+1 < len(grid[0]) \
+        # and grid[row][col+1] == "1" and (row, col+1) not in visitedSet):
+        #     visitStack.append((row,col+1))
+        # if(0 <= row-1 < len(grid) and 0 <= col-1 < len(grid[0]) \
+        # and grid[row][col-1] == "1" and (row, col-1) not in visitedSet):
+        #     visitStack.append((row,col-1))
+        
+            
+
