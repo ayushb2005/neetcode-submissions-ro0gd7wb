f81fcf032d6d30a3d46e7ef8633e3c50dd7ecfe2
Ayush <73355805+ayushb2005@users.noreply.github.com>
2026-04-05 17:06:35 -0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        '''
+        stack to do this, keep appending until end then just 
+        pop off one at a time, and make the new links
+        '''
+        if(not head):
+            return 
+        # stack = []
+        # cur = head
+        # while(cur):
+        #     stack.append(cur)
+        #     cur = cur.next
+        # prev = stack.pop()
+        # head = prev
+        # while(stack):
+        #     popped = stack.pop()
+        #     prev.next = popped
+        #     prev = popped
+        # prev.next = None
+        # return head
+
+        curr = head
+        prev = None
+        while(curr):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
+
+
