d74c4c4a677cb9d7e2477e2a5ed33ed833913c36
Ayush <73355805+ayushb2005@users.noreply.github.com>
2026-07-13 12:30:17 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        self.visit = set()
+        self.hashmap = defaultdict(list)
+        for i in prerequisites:
+            self.hashmap[i[1]].append(i[0])
+
+        def dfs(node):
+            if node in self.visit:
+                return False
+            self.visit.add(node)
+            if self.hashmap[node]:
+                for i in self.hashmap[node]:
+                    if not dfs(i):
+                        return False
+                self.hashmap[node] = []
+            self.visit.remove(node)
+            return True
+            
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+
+        
